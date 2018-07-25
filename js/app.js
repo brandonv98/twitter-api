1a4e186e9cfa6f88389cb6f7851635309b9722ae
Brandon VanCamp <brandon@overtimewebdev.com>
2018-07-25 15:31:48 -0500

    Add axios for server data request, responce

--- js/app.js
+++ js/app.js
@@ -1,5 +1,17 @@
-const people = {
-    firstname: 'any',
-}
+const axios = require('axios');
+const config = require('../config.js');
 
-console.log(people.firstname);
+
+// Make a request for a user with a given ID
+axios.get(`https://api.twitter.com/oauth/${config.access_token}`)
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
