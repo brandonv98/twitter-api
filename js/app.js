63a93118fb496ec832994d293770cacbfbb362bc
Brandon VanCamp <brandon@overtimewebdev.com>
2018-08-07 12:17:07 -0500

    Add static files

--- js/app.js
+++ js/app.js
@@ -10,13 +10,13 @@ config = config.config;
 const app = express();
 
 app.set('view engine', 'pug');
-// app.use(express.static(
-  // 'public'
-// ));
+app.use(express.static(
+  'public'
+));
 
-// app.get('/', (req, res) => {
-  // res.render('index');
-// });
+app.get('/', (req, res) => {
+  res.render('index');
+});
 
 
 
