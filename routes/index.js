829fa71068aab745c5fd5bcb42ac0149ba657aaa
Brandon VanCamp <brandon@overtimewebdev.com>
2018-08-13 18:41:40 -0500

    Add index route

--- routes/index.js
+++ routes/index.js
@@ -18,28 +18,5 @@ router.get('/name', (req, res) => {
   });
   
 
-  // Twitter \\
-// app.get('/', (req, res) => {
-//     // Store reusable data.
-//     const data = {
-//     };
-  
-//   ///  Get user's time line, display the 2 most recent tweets.   /// 
-//       T.get('statuses/user_timeline', { count: 2 }, function(err, data, response) {
-//         data.timeline = data;
-//         data.userName = data[0].user.name;
-//         data.sn = data[0].user.screen_name;
-//         data.userPhoto = data[0].user.profile_image_url;
-//        console.log(data.timeline);
-//        // Render out our passed date to page.
-//        return res.render('index', {
-//          userTimeline: data.timeline[0].text, 
-//          userTimelineNext: data.timeline[1].text, 
-//          userName: data.userName, 
-//          userPhoto: data.userPhoto,
-//          sn: data.sn
-//         });
-//     });
-//   });
   
 module.exports = router;
